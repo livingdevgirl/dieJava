00e53608f8a81c15cb5920a91edd1a730d7f4c99
Emma Dean <emmadeannow@gmail.com>
2017-08-27 20:53:12 -0400

    initial commit

---
+++ src/NoTransportExceptionClass.java
@@ -0,0 +1,6 @@
+public class NoTransportExceptionClass extends RuntimeException {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
---
+++ src/Notification.java
@@ -0,0 +1,18 @@
+public class Notification throw NoTransPortException {
+    public static void main(String[] args) {
+        private createdAt = LocalDateTime{};
+
+        private String subject;
+        private String body;
+
+        try{
+
+        }
+        catch{
+            Exceptions
+        }
+
+    }
+}
+
+
